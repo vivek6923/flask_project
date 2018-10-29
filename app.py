2d30a771fcf56bfa5d7444c955a82eef16afd9be
Vivek Sharma <vivek6923sh@gmail.com>
2018-10-29 12:48:28 +0530

    procfile

--- app.py
+++ app.py
@@ -7,34 +7,36 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+    return 'Hello Heroku'
+# @app.route('/home')
+# def home():
+#     """Renders the home page."""
+#     return render_template(
+#         'index.html',
+#         title='Home Page',
+#         year=datetime.now().year,
+#     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+# @app.route('/contact')
+# def contact():
+#     """Renders the contact page."""
+#     return render_template(
+#         'contact.html',
+#         title='Contact',
+#         year=datetime.now().year,
+#         message='Your contact page.'
+#     )
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+# @app.route('/about')
+# def about():
+#     """Renders the about page."""
+#     return render_template(
+#         'about.html',
+#         title='About',
+#         year=datetime.now().year,
+#         message='Your application description page.'
+#     )
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
