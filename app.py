dec19ce1c8d00878d0df88fa6da664e8c18f19d1
Vivek Sharma <vivek6923sh@gmail.com>
2018-10-29 13:02:46 +0530

    procfile

--- app.py
+++ app.py
@@ -39,9 +39,10 @@ def home():
 #     )
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    # HOST = environ.get('SERVER_HOST', 'localhost')
+    # try:
+    #     PORT = int(environ.get('SERVER_PORT', '5555'))
+    # except ValueError:
+    #     PORT = 5555
+    #app.run(HOST, PORT)
+    app.run()
